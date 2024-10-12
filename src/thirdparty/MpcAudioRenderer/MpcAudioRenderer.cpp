d6364e232174982d0960c94e8020c38178abc997
clsid2 <4704996+clsid2@users.noreply.github.com>
2024-10-12 14:26:41 +0200

    .

--- src/thirdparty/MpcAudioRenderer/MpcAudioRenderer.cpp
+++ src/thirdparty/MpcAudioRenderer/MpcAudioRenderer.cpp
@@ -2366,6 +2366,12 @@ HRESULT CMpcAudioRenderer::CreateRenderClient(WAVEFORMATEX *pWaveFormatEx, const
 		StreamFlags |= AUDCLNT_STREAMFLAGS_EVENTCALLBACK;
 	}
 
+    if (!m_pAudioClient) {
+        // based on crash dump, don't know why this occurs.
+        ASSERT(FALSE);
+        return E_FAIL;
+    }
+
 	if (SUCCEEDED(hr)) {
 		const REFERENCE_TIME hnsPeriodicity = (IsExclusive(pWaveFormatEx) && m_WasapiMethod == WASAPI_METHOD::EVENT) ? m_hnsBufferDuration : 0;
 		hr = m_pAudioClient->Initialize(ShareMode,
