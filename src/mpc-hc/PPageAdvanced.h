6652a41dae6b690a6147f85272ed7104228c631a
adipose <3324395+adipose@users.noreply.github.com>
2026-08-22 12:27:38 -0700

    Fix truncated values in the advanced options list (#4111)

--- src/mpc-hc/PPageAdvanced.h
+++ src/mpc-hc/PPageAdvanced.h
@@ -27,6 +27,7 @@
 #include <memory>
 #include <map>
 #include <deque>
+#include <vector>
 #include "CMPCThemeComboBox.h"
 #include "CMPCThemeSpinButtonCtrl.h"
 #include "CMPCThemePlayerListCtrl.h"
@@ -81,6 +82,9 @@ public:
     bool GetValue() const {
         return currentValue;
     }
+    bool GetDefaultValue() const {
+        return defaultValue;
+    }
     void Apply() {
         settingReference = currentValue;
     }
@@ -116,6 +120,9 @@ public:
     }
     int GetValue() const {
         return currentValue;
+    }
+    int GetDefaultValue() const {
+        return defaultValue;
     }
     void Apply() {
         settingReference = currentValue;
@@ -269,6 +276,9 @@ private:
     CString m_strFalse;
 
     void InitSettings();
+    // The widest value the setting can ever display, and whether it would be drawn flagged (bold).
+    std::pair<CString, bool> GetWidestValue(const std::shared_ptr<SettingsBase>& pItem, CDC* pDC);
+    void AutoSizeValueColumn();
     bool IsDefault(ADVANCED_SETTINGS) const;
     inline bool IsHeaderRow(int iItem) const {
         return m_list.GetItemData(iItem) == HEADER_ITEM_DATA;
